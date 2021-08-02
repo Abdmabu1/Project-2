d1fdc9baaef05df6295b7b7e3a1eec61e2853abb
Chesirekiptui <kbmuraya@gmail.com>
2021-08-03 00:31:40 +0300

    Added travis badge

--- Calculator/Addition.py
+++ Calculator/Addition.py
@@ -1,3 +1,4 @@
+# Addition
 def addition(num1, num2):
     num1 = float(num1)
     num2 = float(num2)
--- Calculator/Division.py
+++ Calculator/Division.py
@@ -1,3 +1,4 @@
+# Division
 def division(num1, num2):
     num1 = float(num1)
     num2 = float(num2)
