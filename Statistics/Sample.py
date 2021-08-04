8e169a6f5f0c07ed302221c8a5401c95ba713cba
Abdmabu1 <85974024+Abdmabu1@users.noreply.github.com>
2021-08-04 22:59:36 +0300

    Update Sample.py

--- Statistics/Sample.py
+++ Statistics/Sample.py
@@ -2,5 +2,5 @@ from random import random
 
 
 def getSample(data, sample_size):
-    random_values = random.sample(data, k=sample_size)
+    random_values = random.sample(data, k=100000)
     return random_values
